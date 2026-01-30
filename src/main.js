1a769f64c4e5a33982c03a09e81d119c21849155
ptrodion <rdnpetrenko@gmail.com>
2026-01-30 20:27:45 +0000

    Added markup for category page

--- src/main.js
+++ src/main.js
@@ -5,3 +5,4 @@ loadPartial('#footer', './partials/footer.html');
 loadPartial('#hero', './partials/main/hero.html');
 loadPartial('#post', './partials/main/post.html');
 loadPartial('#about', './partials/main/about.html');
+loadPartial('#category', './partials/main/category.html');
